3bedbb5bc515198ba5ccfc2749d8d050869fc3ed
Farasat ali <razi97@hotmail.com>
2021-09-13 15:14:13 +0500

    Added prefabs for red and yellow boxes (Some files got left behind)

---
+++ Assets/Scripts/BoxManagement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxManagement : MonoBehaviour
+{
+    [SerializeField]
+    private int boxSize;
+    [SerializeField]
+    private GameObject boxPrefab;
+    [SerializeField]
+    private Transform character;
+    [SerializeField]
+    private Transform charCube;
+
+
+    private int newBoxSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxSize = 0;
+        newBoxSize = boxSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(newBoxSize > boxSize)
+        {
+            int iter = 0;
+            while(newBoxSize > boxSize && iter < 10)
+            {
+                Debug.Log("In the while loop");
+                float boxHeight = 36f + boxSize * (0.8f);
+                Debug.Log(boxHeight);
+                character.position = new Vector3(character.position.x, boxHeight + 4.0f, character.position.z);
+                charCube.position = new Vector3(charCube.position.x, boxHeight + 2.0f, charCube.position.z);
+                Debug.Log(transform.position);
+                GameObject newBoxProperties = Instantiate(boxPrefab, new Vector3(transform.position.x, boxHeight, transform.position.z), Quaternion.identity, transform);
+                Destroy(newBoxProperties.GetComponent<YellowBoxSensor>());
+                newBoxProperties.GetComponent<Rigidbody>().isKinematic = false;
+                newBoxProperties.tag = "Player";
+                boxSize++;
+                iter++;
+            }
+        }
+    }
+
+    public void addBoxes()
+    {
+        newBoxSize++;
+    }
+}
---
+++ Assets/Scripts/RedBoxSensor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class RedBoxSensor : MonoBehaviour
+{
+    [SerializeField]
+    UnityEvent DeathEvent = new UnityEvent();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Player"))
+        {
+            collision.collider.transform.parent = null;
+        }
+        else if(collision.collider.CompareTag("Player Base") || collision.collider.CompareTag("Character"))
+        {
+            Debug.Log("OnCollisionEnter ran here");
+            DeathEvent.Invoke();
+        }    
+
+    }
+}
