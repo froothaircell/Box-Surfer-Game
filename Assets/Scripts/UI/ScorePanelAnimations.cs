22cccebc8662edc885128eb3d963864e3dc2abcc
Farasat ali <razi97@hotmail.com>
2021-10-06 15:54:21 +0500

    modified skybox to match color and added low poly clouds. Also added UI elements for diamonds along with animations

---
+++ Assets/Scripts/UI/ScorePanelAnimations.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class ScorePanelAnimations : MonoBehaviour
+{
+    [SerializeField]
+    private Transform diamondSprite;
+    [SerializeField]
+    private float scaleFactor = 1.5f,
+        transitionDuration = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HeartBeatAnimation()
+    {
+        DOTween.To(
+            () => diamondSprite.localScale,
+            x => diamondSprite.localScale = x,
+            new Vector3(scaleFactor, scaleFactor, 1), transitionDuration)
+            .OnComplete(
+            ()=>
+            {
+                diamondSprite.DOScale(new Vector3(1, 1, 1), transitionDuration);
+            });
+    }
+}
